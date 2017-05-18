73bd765548e933acc249db10a08f9067d2fdb9c6
atiwiex <atiwiex@users.noreply.github.com>
2017-05-17 22:59:52 -0400

    added new cog

---
+++ btcprice.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import requests
+
+class BTC:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def currency(self, ctx, currency:str):
+        """fetches the price of btc in a currency."""
+        url = 'https://blockchain.info/ticker'
+        resp = requests.get(url)
+        btc = resp.json()[currency]
+        await self.bot.say(btc['symbol'] + '' + str(btc['last']))
+
+def setup(bot):
+    n = BTC(bot)
+    bot.add_cog(n)
