237b80c5c61dcc001d561a14d8b2bf470bbf52b8
aikaterna <20862007+aikaterna@users.noreply.github.com>
2017-12-12 14:53:48 -0800

    Add seek command, use pip package

--- music/Music.py
+++ music/Music.py
@@ -2,7 +2,7 @@ import asyncio
 import discord
 import math
 from discord.ext import commands
-from . import lavalink
+from lavalink import lavalink
 
 
 class Music:
@@ -251,6 +251,21 @@ class Music:
 
             songembed = discord.Embed(colour=ctx.guild.me.top_role.colour, title=f"Queued {len(tracks)} track(s).")
             message = await ctx.send(embed=songembed)
+
+    @commands.command()
+    async def seek(self, ctx, seconds: int=5):
+        """Seeks ahead or behind on a track by seconds."""
+        player = await self.lavalink.get_player(guild_id=ctx.guild.id)
+        if not ctx.author.voice or (player.is_connected() and ctx.author.voice.channel.id != int(player.channel_id)):
+            return await self._embed_msg(ctx, "You must be in the voice channel to use seek.")
+        if player.is_playing():
+            if player.current.stream:
+                return await self._embed_msg(ctx, f"Can't seek on a stream.")
+            else:
+                time_sec = seconds * 1000
+                seek = player.position + time_sec
+                await self._embed_msg(ctx, f"Seeking {seconds} seconds...")
+                return await player.seek(seek)
 
     @commands.command()
     async def shuffle(self, ctx):
